ce119060c82268f9e63ab9b56c180ff0477b4341
Fivy <fivysafitri24@gmail.com>
2022-11-05 18:23:46 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const notes = [];
+{
+    id: string,
+    title: string,
+    createdAt: string,
+    updatedAt: string,
+    tags: array of string,
+    body: string,
+   },
+module.exports = notes;
